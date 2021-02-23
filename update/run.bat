67c1ca20502666f7bfd7d46c86ace13109888152
Boshaw <boshaw@protonmail.com>
2021-02-23 15:49:51 -0500

    Updated build for run.bat and ver

--- update/run.bat
+++ update/run.bat
@@ -1,3 +1,3 @@
 mkdir logs
-      java -Xmx1024m -jar hafen-updater.jar update https://gitlab.com/Boshaw/hamster-client/raw/develop/update/ -Djava.util.logging.config.file=logging.properties -Xmx1024M -jar hafen.jar
+java -Xmx1024m -Dhttp.agent="curl/7.51.0" -jar hafen-updater.jar update https://gitlab.com/Boshaw/hamster-client/raw/develop/update/ -Djava.util.logging.config.file=logging.properties -Xmx1024M -jar hafen.jar
     
