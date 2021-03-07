5dd51d070192ce1810bc203e1165aa0ea4a62df2
Boshaw <boshaw@protonmail.com>
2021-03-06 22:30:25 -0500

    Updating KeyBind to track what needs to be done still

--- src/hamster/KeyBind.java
+++ src/hamster/KeyBind.java
@@ -49,58 +49,58 @@ public class KeyBind {
 	    KB_TOGGLE_KIN = addKB("Toggle Kin List", UI_GRP, "C-B");
 	    KB_TOGGLE_OPTS = addKB("Toggle Options", UI_GRP, "C-O");
 	    KB_TOGGLE_CHAT = addKB("Toggle Chat", UI_GRP, "C-C");
-	    KB_TOGGLE_FORAGE = addKB("Toggle Forage", UI_GRP, "S-F");
-	    KB_TOGGLE_LIVESTOCK = addKB("Toggle Livestock Manager", UI_GRP, "S-L");
+	    KB_TOGGLE_FORAGE = addKB("Toggle Forage", UI_GRP, "S-F"); //TODO
+	    KB_TOGGLE_LIVESTOCK = addKB("Toggle Livestock Manager", UI_GRP, "S-L"); //TODO
 	    KB_TOGGLE_CMD = addKB("Toggle Console", UI_GRP, "S-Semicolon");
-	    KB_TOGGLE_PROFILER = addKB("Toggle Profiler", UI_GRP, "C-L");
+	    KB_TOGGLE_PROFILER = addKB("Toggle Profiler", UI_GRP, "C-L"); //TODO fix
 	    KB_FOCUS_MAP = addKB("Focus Map", UI_GRP, "Escape");
 	    KB_RECALL_MAP_ONE = addKB("Recall First map pos/size", UI_GRP, "C-Z");
 	    KB_RECALL_MAP_TWO = addKB("Recall Second map pos/size", UI_GRP, "C-X");
-	    KB_LOCK_ITEM_ON_MOUSE = addKB("Lock item on mouse", UI_GRP, "Back Quote");
+	    KB_LOCK_ITEM_ON_MOUSE = addKB("Lock item on mouse", UI_GRP, "Back Quote"); //TODO
 	    KB_SCREENSHOT = addKB("Screenshot", UI_GRP, "M-S");
 	}
 	{ // Gameplay Keybinds
 	    final String GP_GRP = "Gameplay";
-	    KB_TOGGLE_GRID = addKB("Toggle Grid Lines", GP_GRP, "C-G");
-	    KB_TOGGLE_TIPS = addKB("Toggle Hovertips", GP_GRP, "C-Q");
-	    KB_TOGGLE_HITBOXES = addKB("Toggle Hitboxes", GP_GRP, "C-H");
-	    KB_TOGGLE_HIDDEN = addKB("Toggle Gob When Hidden", GP_GRP, "S-G");
+	    KB_TOGGLE_GRID = addKB("Toggle Grid Lines", GP_GRP, "C-G"); //TODO
+	    KB_TOGGLE_TIPS = addKB("Toggle Hovertips", GP_GRP, "C-Q"); //TODO
+	    KB_TOGGLE_HITBOXES = addKB("Toggle Hitboxes", GP_GRP, "C-H"); //TODO
+	    KB_TOGGLE_HIDDEN = addKB("Toggle Gob When Hidden", GP_GRP, "S-G"); //TODO
 	    KB_TOGGLE_PAUSE = addKB("Toggle Pause", GP_GRP, "C-P");
-	    KB_QUICK_ACTION = addKB("Quick Action", GP_GRP, "Q");
-	    KB_QUICK_BOARD = addKB("Quick Board", GP_GRP, "S-Q");
-	    KB_EQ_HELD_INTO_LH = addKB("Equip Held Item Into Left Hand", GP_GRP, "M-Z");
-	    KB_EQ_HELD_INTO_RH = addKB("Eqip Held Item Into Right Hand", GP_GRP, "M-X");
+	    KB_QUICK_ACTION = addKB("Quick Action", GP_GRP, "Q"); //TODO
+	    KB_QUICK_BOARD = addKB("Quick Board", GP_GRP, "S-Q"); //TODO
+	    KB_EQ_HELD_INTO_LH = addKB("Equip Held Item Into Left Hand", GP_GRP, "M-Z"); //TODO
+	    KB_EQ_HELD_INTO_RH = addKB("Eqip Held Item Into Right Hand", GP_GRP, "M-X"); //TODO
 	}
 	{ // Movement Keybinds
 	    final String MV_GRP = "Movement";
-	    KB_CYCLE_SPEED = addKB("Cycle Character Speed", MV_GRP, "C-R");
-	    KB_CRAWL = addKB("Change to Crawl Speed", MV_GRP, "M-Q");
-	    KB_WALK = addKB("Change to Walk Speed", MV_GRP, "M-W");
-	    KB_RUN = addKB("Change to Run Speed", MV_GRP, "M-E");
-	    KB_SPRINT = addKB("Change to Sprint Speed", MV_GRP, "M-R");
-	    KB_MOVE_NORTH = addKB("Move North", MV_GRP, "Up");
-	    KB_MOVE_SOUTH = addKB("Move South", MV_GRP, "Down");
-	    KB_MOVE_EAST = addKB("Move East", MV_GRP, "Left");
-	    KB_MOVE_WEST = addKB("Move West", MV_GRP, "Right");
+	    KB_CYCLE_SPEED = addKB("Cycle Character Speed", MV_GRP, "C-R"); //TODO
+	    KB_CRAWL = addKB("Change to Crawl Speed", MV_GRP, "M-Q"); //TODO
+	    KB_WALK = addKB("Change to Walk Speed", MV_GRP, "M-W"); //TODO
+	    KB_RUN = addKB("Change to Run Speed", MV_GRP, "M-E"); //TODO
+	    KB_SPRINT = addKB("Change to Sprint Speed", MV_GRP, "M-R"); //TODO
+	    KB_MOVE_NORTH = addKB("Move North", MV_GRP, "Up"); //TODO
+	    KB_MOVE_SOUTH = addKB("Move South", MV_GRP, "Down"); //TODO
+	    KB_MOVE_EAST = addKB("Move East", MV_GRP, "Left"); //TODO
+	    KB_MOVE_WEST = addKB("Move West", MV_GRP, "Right"); //TODO
 	}
 	{ // Combat Keybinds
 	    final String CBT_GRP = "Combat";
-	    KB_AGGRO_NEAREST_ANIMAL_TO_MOUSE = addKB("Aggro animal nearest to mouse", CBT_GRP, "C-F");
-	    KB_AGGRO_NEAREST_PLAYER_TO_MOUSE = addKB("Aggro player nearest to mouse", CBT_GRP, "C-D");
-	    KB_TARGET_NEAREST_ANIMAL_TO_MOUSE = addKB("Target nearest animal to mouse", CBT_GRP, "S-A");
-	    KB_TARGET_NEAREST_PLAYER_TO_MOUSE = addKB("Target nearest player to mouse", CBT_GRP, "S-D");
-	    KB_AGGRO_TARGET = addKB("Aggro targeted gob", CBT_GRP, "S-T");
-	    KB_TARGET_CURRENT = addKB("Target current gob", CBT_GRP, "S-C");
-	    KB_PEACE_CURRENT = addKB("Peace current target", CBT_GRP, "S-P");
+	    KB_AGGRO_NEAREST_ANIMAL_TO_MOUSE = addKB("Aggro animal nearest to mouse", CBT_GRP, "C-F"); //TODO
+	    KB_AGGRO_NEAREST_PLAYER_TO_MOUSE = addKB("Aggro player nearest to mouse", CBT_GRP, "C-D"); //TODO
+	    KB_TARGET_NEAREST_ANIMAL_TO_MOUSE = addKB("Target nearest animal to mouse", CBT_GRP, "S-A"); //TODO
+	    KB_TARGET_NEAREST_PLAYER_TO_MOUSE = addKB("Target nearest player to mouse", CBT_GRP, "S-D"); //TODO
+	    KB_AGGRO_TARGET = addKB("Aggro targeted gob", CBT_GRP, "S-T"); //TODO
+	    KB_TARGET_CURRENT = addKB("Target current gob", CBT_GRP, "S-C"); //TODO
+	    KB_PEACE_CURRENT = addKB("Peace current target", CBT_GRP, "S-P"); //TODO
 	    final int moves = 10;
 	    KB_FIGHT_MOVE = new KeyBind[moves];
 	    for(var i = 1; i < moves; ++i) {
-		KB_FIGHT_MOVE[i-1] = addKB("Fight Move " + i, CBT_GRP, Integer.toString(i));
+		KB_FIGHT_MOVE[i-1] = addKB("Fight Move " + i, CBT_GRP, Integer.toString(i)); //TODO
 	    }
 	}
 	{ // Camera Keybinds
 	    final String CAM_GRP = "Camera";
-	    KB_RECENTER_CAMERA = addKB("Recenter Camera on Player", CAM_GRP, "S-C");
+	    KB_RECENTER_CAMERA = addKB("Recenter Camera on Player", CAM_GRP, "S-C"); //TODO
 	}
 	{ // Setup Hotkey keybinds
 	    final String HB_GRP = "Hotbar";
