987d158c1d8bb2e23b8bf50430ca364213d32b34
Boshaw <boshaw@protonmail.com>
2021-03-22 00:24:05 -0400

    Updated updater and changed how the hasher bit of it works

--- update/run-and-update.bat
+++ update/run-and-update.bat
@@ -1,2 +1,3 @@
-java -Djava.util.logging.config.file=updater-logging.properties -Xmx1024m -Dhttp.agent="curl/7.51.0" -jar hamster-client-updater.jar update https://gitlab.com/Boshaw/hamster-client/raw/master/update/
+java -Djava.util.logging.config.file=updater-logging.properties -Xmx1024m -Dhttp.agent="curl/7.51.0" -jar hamster-client-updater-1.0-shaded.jar update https://gitlab.com/Boshaw/hamster-client/raw/master/update/
 java -Djava.util.logging.config.file=logging.properties -Xmx1024M -jar hafen.jar
+pause
