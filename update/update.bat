52e100ee49bea258f1fea0b4bec746653be72636
Boshaw <boshaw@protonmail.com>
2021-03-21 23:26:59 -0400

    Updated to use new updater to allow multiple configs

---
+++ update/update.bat
@@ -0,0 +1,1 @@
+java -Djava.util.logging.config.file=updater-logging.properties -Xmx1024m -Dhttp.agent="curl/7.51.0" -jar hamster-client-updater.jar update https://gitlab.com/Boshaw/hamster-client/raw/master/update/
