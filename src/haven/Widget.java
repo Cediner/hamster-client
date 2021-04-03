1334aa6d39c5a6ff89b6a5542b59237afeb662b8
Boshaw <boshaw@protonmail.com>
2021-04-03 08:40:47 -0400

    Bug fix on parentpos

--- src/haven/Widget.java
+++ src/haven/Widget.java
@@ -524,7 +524,7 @@ public class Widget {
     }
 	
     public Coord parentpos(Widget in) {
-	if(in == this)
+	if(in == this || parent == null)
 	    return(new Coord(0, 0));
 	return(parent.xlate(parent.parentpos(in).add(c), true));
     }
