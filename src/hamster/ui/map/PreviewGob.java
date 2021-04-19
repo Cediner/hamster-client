900a0c66afb40ba6469dac5a033000bd15b77a3b
Boshaw <boshaw@protonmail.com>
2021-04-18 23:23:02 -0400

    Some fixes to Gob Previewer

--- src/hamster/ui/map/PreviewGob.java
+++ src/hamster/ui/map/PreviewGob.java
@@ -6,15 +6,18 @@ import haven.render.Pipe;
 public class PreviewGob extends Gob {
     public PreviewGob(final Gob org) {
 	super(org.glob, new Coord2d(0, 0));
-	for(GAttrib a : org.attr.values()) {
-	    if(a instanceof ResDrawable) {
-		setattr(((ResDrawable)a).cloneFor(this));
-	    } else if(a instanceof Composite) {
-		setattr(((Composite)a).cloneFor(this));
+	synchronized (this) {
+	    for (GAttrib a : org.attr.values()) {
+		if (a instanceof ResDrawable) {
+		    setattr(((ResDrawable) a).cloneFor(this));
+		} else if (a instanceof Composite) {
+		    setattr(((Composite) a).cloneFor(this));
+		}
 	    }
-	}
-	for(Gob.Overlay ol : org.ols) {
-	    addol(new Overlay(this, ol.id, ol.res, ol.sdt != null ? ol.sdt.clone() : null));
+	    for (Gob.Overlay ol : org.ols) {
+	        if(ol.res != null)
+	            addol(new Overlay(this, ol.id, ol.res, ol.sdt != null ? ol.sdt.clone() : new MessageBuf()));
+	    }
 	}
     }
 
