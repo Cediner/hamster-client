3275be1e60dd0467fa9c2e9308c9993d405f2d3d
Boshaw <boshaw@protonmail.com>
2021-05-07 21:28:39 -0400

    -a
    Updated files for 1.1.0

--- update/data/scripts/lib/_common_scout.lisp
+++ update/data/scripts/lib/_common_scout.lisp
@@ -5,28 +5,55 @@
         :hafen-config))
 (in-package :scout-common)
 
-
 (defconstant +alert-refresh+ (* 3 60 1000))
 (defconstant +targets+
   `("gfx/terobjs/vehicle/knarr"
     "gfx/terobjs/vehicle/snekkja"
     "gfx/terobjs/vehicle/rowboat"))
 
+(defclass target ()
+  ((id :initarg :id :accessor target-id)
+   (name :initarg :name :accessor target-name)
+   (init-rc :initarg :init-rc :accessor target-init-rc)
+   (rc :initarg :rc :accessor target-rc)
+   (angle :initarg :angle :accessor target-angle)
+   (avg-speed :initarg :avg-speed :accessor target-avg-speed)
+   (max-speed :initarg :max-speed  :accessor target-max-speed)
+   (spotted-at :initarg :spotted-at  :accessor target-spotted-at)
+   (last-alert :initarg :last-alert :accessor target-last-alert)
+   (tick :initarg :tick :accessor target-tick)
+   (changed :initarg :changed :accessor target-changed)))
 
-(defstruct spotted-gob
-  id
-  name
-  first-spotted-at
-  avg-speed
-  max-speed
-  gear
-  first-rc-at
-  last-rc-at
-  last-spotted
-  last-alert
-  last-angle
-  tick)
+(defgeneric describe (target))
+(defgeneric update (target gob tick))
+(defun update-target (target tick rc angle speed)
+  (setf (target-rc target) rc)
+  (setf (target-angle target) angle)
+  (setf (target-avg-speed target)
+        (/ (+ (target-avg-speed target) speed) 2))
+  (setf (target-max-speed target) (max (target-max-speed target) speed))
+  (setf (target-tick target) tick))
 
+(defclass player (target)
+  ((gear :initarg :gear :accessor player-gear)))
+
+(defun make-player (gob tick)
+  (make-instance 'player
+                 :id (gob-id gob) :name (gob-kinname gob)
+                 :init-rc (gob-rc gob) :rc (gob-rc gob) :angle (gob-angle gob)
+                 :avg-speed (gob-v gob) :max-speed (gob-v gob)
+                 :spotted-at (get-time) :last-alert (get-time)
+                 :tick tick :changed nil :gear (gob-equipment gob)))
+
+(defun update-player (player tick rc angle speed gear)
+  (update-target player tick rc angle speed)
+  (setf (target-changed player) (not (string= (describe-gear (player-gear player)) (describe-gear gear))))   
+  (setf (player-gear player) gear))
+
+(defmethod update ((target player) gob tick)
+  (update-player target tick
+                 (gob-rc gob) (gob-angle gob) (gob-v gob)
+                 (gob-equipment gob)))
 
 (defun describe-gear (gear)
   (if gear
@@ -34,7 +61,58 @@
              (loop
               for equ in (listify gear)
               collect (format nil "~A~%" equ)))
-      ""))
+    ""))
+
+(defmethod describe ((target player))
+  (format nil "~A [~A] [Avg Speed ~A] [Max Speed ~A] [Duration ~A] Wearing:~%~A"
+          (target-name target)
+          (target-id target)
+          (target-avg-speed target)
+          (target-max-speed target)
+          (- (get-time) (target-spotted-at target))
+          (describe-gear (player-gear target))))
+
+(defclass boat (target)
+  ((sail :initarg :sail :accessor boat-sail)))
+
+(defun make-boat (gob tick)
+  (make-instance 'boat
+                 :id (gob-id gob) :name (gob-name gob)
+                 :init-rc (gob-rc gob) :rc (gob-rc gob) :angle (gob-angle gob)
+                 :avg-speed (gob-v gob) :max-speed (gob-v gob)
+                 :spotted-at (get-time) :last-alert (get-time)
+                 :tick tick :changed nil :sail (gob-sail gob)))
+
+(defmethod describe ((target boat))
+  (format nil "~A [~A] [Avg Speed ~A] [Max Speed ~A] [Duration ~A]"
+          (target-name target)
+          (target-id target)
+          (target-avg-speed target)
+          (target-max-speed target)
+          (- (get-time) (target-spotted-at target))))
+
+(defun update-boat (boat tick rc angle speed sail)
+  (update-target boat tick rc angle speed)
+  (setf (target-changed boat) (or (and (not (boat-sail boat)) sail)))
+  (setf (boat-sail boat) sail))
+
+(defmethod update ((target boat) gob tick)
+  (update-boat target tick
+               (gob-rc gob) (gob-angle gob) (gob-v gob)
+               (gob-sail gob)))
+
+(defun make-target (gob tick)
+  (if(gob-has-tag gob +human+)
+    (make-player gob tick)
+    (make-boat gob tick)))
+
+(defun holding-bad-people (gob idmap)
+  (when (plusp (gob-how-many-gobs-held gob))
+    (let ((heldgobs (gob-held-gobs gob)))
+      (doarr (held heldgobs)
+        (when (gethash held idmap)
+          (return-from holding-bad-people t))))
+    nil))
 
 (defun scan-for-targets (role spotted-gobs tick)
   (let ((targets (gob-get-all-by-filter
@@ -42,104 +120,60 @@
                     (or (member (gob-name gob) +targets+ :test 'equal)
                         (and (gob-has-tag gob +human+)
                              (not (is-gob-friendly gob))
-                             (not (= (gob-id gob) (mv-plgob (mv))))))))))
-    (loop
-       for target in targets
-       do (let ((name (if (gob-has-tag target +human+) (gob-kinname target) (gob-name target)))
-                (gear (if (gob-has-tag target +human+) (gob-equipment target) nil)))
-            (cond
-              ((and (gethash (gob-id target) spotted-gobs)
-                    (> (- (get-time)
-                          (spotted-gob-last-alert (gethash (gob-id target) spotted-gobs)))
-                       +alert-refresh+))
-               (let ((gob (gethash (gob-id target) spotted-gobs)))
-                 (setf (spotted-gob-last-spotted gob) (gob-rc target))
-                 (setf (spotted-gob-last-rc-at gob) (gob-rc target))
-                 (setf (spotted-gob-last-angle gob) (gob-angle target))
-                 (setf (spotted-gob-avg-speed gob)
-                       (/ (+ (spotted-gob-avg-speed gob) (gob-v target)) 2))
-                 (setf (spotted-gob-max-speed gob)
-                       (max (spotted-gob-avg-speed gob) (gob-v target)))
-                 (setf (spotted-gob-tick gob) tick)
-                 (setf (spotted-gob-last-alert gob) (get-time))
-                 (send-discord-message-with-map-and-mark
-                  "player-alerts" 
-                  (format nil "<@&~A> Still spotted a ~A [~A] [Avg Speed ~A] [Max Speed ~A]"
-                          role name (gob-id target)
-                          (spotted-gob-avg-speed gob) (spotted-gob-max-speed gob))
-                  (gob-rc target)
-                  (gob-angle target))))
-              ((gethash (gob-id target) spotted-gobs)
-               (let ((gob (gethash (gob-id target) spotted-gobs)))
-                 (setf (spotted-gob-last-spotted gob) (gob-rc target))
-                 (setf (spotted-gob-last-rc-at gob) (gob-rc target))
-                 (setf (spotted-gob-last-angle gob) (gob-angle target))
-                 (unless (equalp (spotted-gob-gear gob) gear)
-                   (setf (spotted-gob-gear gob) gear)
-                   (send-discord-message
-                    "player-alerts"
-                    (format nil "<@&~A> Gob ~A [~A] is now wearing ~A~%"
-                            role
-                            (spotted-gob-name gob)
-                            (spotted-gob-id gob)
-                            (describe-gear (spotted-gob-gear gob)))))
-                 (setf (spotted-gob-avg-speed gob)
-                       (/ (+ (spotted-gob-avg-speed gob) (gob-v target)) 2))
-                 (setf (spotted-gob-max-speed gob)
-                       (max (spotted-gob-avg-speed gob) (gob-v target)))
-                 (setf (spotted-gob-tick gob) tick)))
-              (t ;;new target
-               (let ((gob (make-spotted-gob :id (gob-id target)
-                                            :name name
-                                            :avg-speed (gob-v target)
-                                            :max-speed (gob-v target)
-                                            :gear gear
-                                            :first-spotted-at (get-time)
-                                            :first-rc-at (gob-rc target)
-                                            :last-spotted (gob-rc target)
-                                            :last-angle (gob-angle target)
-                                            :last-alert (get-time)
-                                            :tick tick)))
-                 (setf (gethash (spotted-gob-id gob) spotted-gobs) gob)
-                 (send-discord-message-with-map-and-mark
-                  "player-alerts"
-                  (format nil
-                          (if gear
-                              "<@&~A> Spotted a ~A [~A] going ~A tiles/s Wearing:~%~A"
-                            "<@&~A> Spotted a ~A [~A] going ~A tiles/s ~A")
-                          role
-                          name
-                          (gob-id target)
-                          (spotted-gob-max-speed gob)
-                          (describe-gear (spotted-gob-gear gob)))
-                  (gob-rc target)
-                  (gob-angle target)))))))))
-
-(defun describe-missing-gob (gob)
-  (let* ((first-pos (spotted-gob-first-rc-at gob))
-         (last-pos (spotted-gob-last-rc-at gob))
-         (real-loc (get-real-location-on-map (spotted-gob-last-spotted gob))))
-    (format nil "~A [~A] [Avg Speed ~A] [Max Speed ~A] [Duration ~A]"
-            (spotted-gob-name gob)
-            (spotted-gob-id gob)
-            (spotted-gob-avg-speed gob)
-            (spotted-gob-max-speed gob)
-            (- (get-time) (spotted-gob-first-spotted-at gob)))))
-
-(defun check-if-any-spotted-have-left (role spotted-gobs tick)
-  (loop
-     for id being the hash-keys in spotted-gobs
-     when (/= tick (spotted-gob-tick (gethash id spotted-gobs)))
-     do (let ((gob (gethash id spotted-gobs)))
-          (remhash id spotted-gobs)
-          (let ((msg (describe-missing-gob gob)))
+                             (not (= (gob-id gob) (mv-plgob (mv)))))))))
+        (idmap (make-hash-table)))
+    ;; Populate the idmap
+    (dolist (target targets)
+      (setf (gethash (gob-id target) idmap) target))
+    ;; Scan targets
+    (dolist (gob targets)
+      (let ((target (gethash (gob-id gob) spotted-gobs)))
+        (cond
+         ((and target
+               (> (- (get-time) (target-last-alert target)) +alert-refresh+))
+          ;; Previously seen Gob that hasn't left our view by the +alert-refresh+ time
+          (update target gob tick) 
+          (send-discord-message-with-map-and-mark
+           "player-alerts"
+           (format nil "<@&~A> Stilled spotted a ~A" role (describe target))
+           (target-rc target)
+           (target-angle target)))
+         (target
+          ;; Previously seen Gob that hasn't reached the alert-refresh yet
+          (update target gob tick)
+          (when (target-changed target)
+            (if (typep target 'player)
+                (send-discord-message "player-alerts" (format nil "<@&~A> Target changed: ~A" role (describe target)))
+              (when (and (typep target 'boat)
+                         (boat-sail target))
+                (send-discord-image "player-alerts" (format nil "<@&~A> Custom boat sail for ~A" role (target-id target))
+                                    (boat-sail target))))))
+                
+         (t
+          ;; Brand new target, don't add if they aren't human and being held or have only friendly passengers
+          (when (or (gob-has-tag gob +human+)
+                    (and (not (gob-is-held-by-something gob))
+                         (holding-bad-people gob idmap)))
+            (setf target (make-target gob tick))
+            (setf (gethash (target-id target) spotted-gobs) target)
             (send-discord-message-with-map-and-mark
              "player-alerts"
-             (format nil "<@&~A> Last spotted a ~A" role msg)
-             (spotted-gob-last-rc-at gob)
-             (spotted-gob-last-angle gob))))))
+             (format nil "<@&~A> Spotted a ~A" role (describe target))
+             (target-rc target)
+             (target-angle target))
+            (when (and (typep target 'boat)
+                       (boat-sail target))
+              (send-discord-image "player-alerts" "Custom boat sail" (boat-sail target))))))))
+    ;; Cleanup old targets that are no longer spotted
+    (loop
+     for id being the hash-keys in spotted-gobs
+     when (/= tick (target-tick (gethash id spotted-gobs)))
+     do (let ((gob (gethash id spotted-gobs)))
+          (remhash id spotted-gobs)
+          (send-discord-message-with-map-and-mark
+           "player-alerts"
+           (format nil "<@&~A> Last spotted a ~A" role (describe gob))
+           (target-rc gob)
+           (target-angle gob))))))
 
-(export '(scan-for-targets
-          describe-missing-gob
-          spotted-gob
-          check-if-any-spotted-have-left))
+(export '(scan-for-targets))
